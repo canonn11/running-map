e25d591833d59f9a6cc0fbd287b38e481e421b98
canonn11 <myungkwanokk@gmail.com>
2022-09-06 20:37:13 +0900

    TailwindCSS 적용

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -18,7 +18,7 @@
     <Counter />
   </div>
 
-  <p>
+  <p class = "text-black">
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
 
@@ -27,19 +27,3 @@
   </p>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
